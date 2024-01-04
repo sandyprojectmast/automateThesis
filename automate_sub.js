22a1879eddce7cc1de0c0e5d64d601a7930ceab3
tessariswara <tessa.projectmast@outlook.com>
2024-01-04 11:13:10 +0000

    initial add file js

---
+++ automate_sub.js
@@ -0,0 +1,21 @@
+import mqtt from 'mqtt';
+const topics = ['SN123', 'SN124', 'SN125']; // Add or remove topics as needed
+
+const client = mqtt.connect('mqtt://localhost', {
+  username: 'mqtt_pass',
+  password: 'thesismania',
+});
+
+client.on('connect', () => {
+  topics.forEach((topic) => {
+    client.subscribe(topic, (err) => {
+      if (!err) {
+        console.log(`Subscribed to ${topic}`);
+      }
+    });
+  });
+});
+
+client.on('message', (topic, message) => {
+  console.log(`Received message on topic ${topic}: ${message.toString()}`);
+});
